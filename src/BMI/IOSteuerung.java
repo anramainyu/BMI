993339e0a9d0bfd9ae5518ab939afa8b8af4235c
anramainyu <stephanschiefer@web.de>
2015-02-13 11:19:19 +0100

    IOsteuerung erstellt

---
+++ src/BMI/IOSteuerung.java
@@ -0,0 +1,79 @@
+package BMI;
+
+import java.util.Scanner;
+
+/**
+ * Created by sschiefer on 12.02.2015.
+ */
+public class IOSteuerung
+{
+    double groesse;
+    double gewicht;
+
+    // Eingaben
+
+
+
+    //Ausgaben
+    public void ausgabeGewichtInfo()
+    {
+        System.out.println("Zulässige Werte für das Gewicht sind 25 bis 500" +
+                " Kilo. Bitte geben Sie Ihr Gewicht neu ein.");
+    }
+    public void ausgabeGroesseInfo()
+    {
+        System.out.println("Zulässige Werte für die Größe sind 125 cm und 260 cm");
+    }
+    public void ioGewicht()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte geben Sie das Gewicht in Kilogramm an:");
+        double g = sc.nextDouble();
+
+        if(sc!=null)
+        {
+            if(g >= 25 && g<=500)
+            {
+                this.gewicht=g;
+            }
+            else
+            {
+                ausgabeGewichtInfo();
+            }
+        }
+        else
+        {
+            ausgabeGewichtInfo();
+        }
+    }
+    public void ioGroesse()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte geben Sie die Größe in Zentimeter an:");
+        double g = sc.nextDouble();
+
+        if(sc!=null)
+        {
+            if(g >= 125 && g <=500)
+            {
+                this.groesse=g;
+            }
+            else
+            {
+                ausgabeGroesseInfo();
+            }
+        }
+        else
+        {
+            ausgabeGroesseInfo();
+        }
+    }
+
+    public double getGroesse() {
+        return groesse;
+    }
+
+    public double getGewicht() {
+        return gewicht;
+    }
+}
--- src/IOSteuerung.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by sschiefer on 13.02.2015.
- */
-public class IOSteuerung {
-}
